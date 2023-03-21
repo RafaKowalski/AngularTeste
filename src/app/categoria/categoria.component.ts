3fdf6c079bf4640fd6c9516d0087c5a45daf7a42
Rafael Kowalski <rafaelmk06@gmail.com>
2023-03-21 15:35:52 -0300

    Configurando categoria.component para receber dados da api

--- src/app/categoria/categoria.component.ts
+++ src/app/categoria/categoria.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/model/Categoria';
+import { ApiService } from 'src/Services/api.service';
 
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
   styleUrls: ['./categoria.component.scss']
 })
-export class CategoriaComponent {
+export class CategoriaComponent implements OnInit {
+  
+  displayedColumns: string[] = ['nome', 'imagem', 'acao'];
+  dataSource: Categoria[] = [];
+  isLoadingResults = true;
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit()  {
+    this.api.getCategorias()
+    .subscribe(res => {
+      this.dataSource = res;
+      console.log(this.dataSource);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
+  }
 
 }
+
+ 
+
